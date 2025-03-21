d5bc23507c99df5eb983382eb8872a66de4838b4
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-03-21 23:12:09 +0600

    add blog page header

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,4 +1,11 @@
+import PageHeader from "@/components/shared/PageHeader"
+
 const page = () => {
-  return <div className='h-[30vh]'>Blogs</div>
+  return <div className=''>
+    <PageHeader
+      title={'Transforming Healthcare with Innovation: The Blog'}
+      subtitle={'Stay ahead with expert insights, groundbreaking research, and the latest advancements in AI, digital health, and medical technology.'}
+    />
+  </div>
 }
 export default page
--- components/shared/PageHeader.jsx
+++ components/shared/PageHeader.jsx
@@ -5,14 +5,16 @@ const PageHeader = ({ title, subtitle }) => {
   const path = usePathname()
   const formattedPath = path.slice(1).charAt(0).toUpperCase() + path.slice(2)
   return (
-    <div className='bg-gradient-to-bl from-white to-[#F5FDFF] pt-12 px-12 xl:px-36 flex flex-col justify-start items-start gap-4'>
-      <h1 className='text-titleSubtitle text-[48px] font-[700] leading-[64px] w-3/4 '>
-        {title}
-      </h1>
-      <p className='text-paragraph text-[20px] font-[400] leading-[32px]  w-2/4'>
-        {subtitle}
-      </p>
-      <div className=' pt-[48px] w-full'>
+    <div className=' pt-12 px-12 xl:px-36 bg-gradient-to-bl from-white to-[#F5FDFF]'>
+      <div className=' flex flex-col justify-start items-start gap-4 w-3/5'>
+        <h1 className='text-titleSubtitle text-[48px] font-[700] leading-[64px'>
+          {title}
+        </h1>
+        <p className='text-paragraph text-[20px] font-[400] leading-[32px]'>
+          {subtitle}
+        </p>
+      </div>
+      <div className=' pt-[64px] w-full'>
         <p className='border-t border-b border-t-[#F1F3F4] border-b-[#F1F3F4] w-full py-[16px] leading-[24px] text-[16px] font-[500] flex justify-start items-center gap-[16px]'>
           <span className='text-paragraph'>Home</span>{' '}
           <span className='text-[#F05232]'>/</span>{' '}
