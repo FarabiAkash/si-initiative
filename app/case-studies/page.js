cd8dc2362fcdb7cbbaeb0912f2816c0ba8cd92eb
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-03-21 23:08:04 +0600

    add case-studies header

--- app/case-studies/page.js
+++ app/case-studies/page.js
@@ -1,4 +1,11 @@
+import PageHeader from "@/components/shared/PageHeader"
+
 const page = () => {
-  return <div className='h-[30vh]'>Case Studies</div>
+  return <div className=''>
+    <PageHeader
+      title={'Transforming Healthcare with Proven Solutions'}
+      subtitle={'Explore how our cutting-edge technology has improved patient outcomes, optimized workflows, and driven healthcare innovation'}
+    />
+  </div>
 }
 export default page
