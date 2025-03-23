29d724f326dd518a868228029f2ad7d6741ff525
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-03-24 00:04:21 +0600

    Initialize Work Process in the service page

---
+++ components/services/WorkProcess.jsx
@@ -0,0 +1,16 @@
+const WorkProcess = () => {
+  return (
+    <div className='custom-container'>
+      <div className='flex flex-col justify-center items-center text-center gap-4'>
+        <h1 className='text-titleSubtitle text-[32px] font-[700] leading-[40px]'>
+          Our Work Process
+        </h1>
+        <p className='text-paragraph text-[18px] leading-[24px] font-[400] w-[40vw]'>
+          Streamlined, research-driven, and technology-powered—our process
+          ensures seamless execution and impactful healthcare solutions.
+        </p>
+      </div>
+    </div>
+  )
+}
+export default WorkProcess
