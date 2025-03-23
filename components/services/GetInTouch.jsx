d1b94b697565cf74479b2a23e037aecfecae93e5
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-03-24 00:04:42 +0600

    Implement Get In Touch Section in service page

---
+++ components/services/GetInTouch.jsx
@@ -0,0 +1,16 @@
+const GetInTouch = () => {
+  return (
+    <div className='custom-container bg-primary flex flex-col items-center justify-center gap-[16px] text-white !py-[100px]'>
+      <h2 className='text-[32px] font-[700] leading-[40px]'>
+        Ready to Transform Healthcare with Us?
+      </h2>
+      <p className='text-[18px] font-[400] leading-[24px]'>
+        Let’s collaborate to create impactful digital healthcare solutions.
+      </p>
+      <button className='mt-10 h-[56px] py-[16px] px-[40px] rounded-[28px] bg-[#F05232] text-white text-[14px] font-[600] tracking-[1.4px] uppercase'>
+        Get in Touch
+      </button>
+    </div>
+  )
+}
+export default GetInTouch
