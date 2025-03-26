3540b50ce497d42dcd0e7bdbb3b290aabb1bb3c7
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-03-27 03:15:25 +0600

    make responsive the about header

--- components/About-us/AboutHeader.jsx
+++ components/About-us/AboutHeader.jsx
@@ -2,7 +2,7 @@ const AboutHeader = () => {
   return (
     <div className='custom-container'>
       {/* Breadcrumb */}
-      <div className='w-full h-[56px] flex items-center border-y border-[#F1F3F4]'>
+      <div className='w-full min-h-[56px] flex items-center border-y border-[#F1F3F4]'>
         <p className='text-[16px] font-medium flex items-center gap-4'>
           <span className='text-paragraph'>Home</span>
           <span className='text-[#F05232]'>/</span>
@@ -12,14 +12,14 @@ const AboutHeader = () => {
 
       {/* Hero Section */}
       <div
-        className=' sm:relative mt-24 p-6 rounded-[16px] flex flex-col md:flex-row items-center md:items-start'
+        className='xl:relative mt-6 lg:mt-24 p-6 rounded-[16px] flex flex-col-reverse lg:flex-row items-center  xl:items-start'
         style={{
           background:
             'linear-gradient(109deg, #F7FDFF 0%, #E3F1F7 41.07%, #C4E3F2 71.08%)'
         }}
       >
-        <div className='md:w-2/4 pl-[80px] py-[72px]'>
-          <h1 className='text-[28px] md:text-[48px] font-bold leading-[64px] text-titleSubtitle'>
+        <div className='lg:w-2/4 pt-6 lg:pt-0 pl-0 xl:pl-[80px] xl:py-[72px]'>
+          <h1 className='text-[28px] md:text-[32px] lg:text-[48px] font-bold sm:leading-[36px] lg:leading-[64px] text-titleSubtitle'>
             Revolutionizing Healthcare Through Innovation
           </h1>
           <p className=' text-paragraph text-[20px] font-[400] leading-[32px]'>
@@ -28,7 +28,7 @@ const AboutHeader = () => {
           </p>
         </div>
         {/* Positioned Image */}
-        <div className='sm:absolute top-[-30%] right-10 sm:w-[496px] sm:h-[496px] bg-[#D9D9D9] rounded-[8px]'></div>
+        <div className='xl:absolute top-[-30%] right-10 w-full lg:w-[496px] h-[300px] sm:h-[496px] bg-[#D9D9D9] rounded-[8px]'></div>
       </div>
     </div>
   )
