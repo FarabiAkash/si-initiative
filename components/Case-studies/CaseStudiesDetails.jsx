e9f58225d3aabb675f28f8c45a4e2b9223663e82
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-03-27 00:34:05 +0600

    Complete case studies details page with all device responsive

--- components/Case-studies/CaseStudiesDetails.jsx
+++ components/Case-studies/CaseStudiesDetails.jsx
@@ -120,6 +120,20 @@ const CaseStudiesDetails = ({ caseStudy }) => {
             </span>
           </div>
         </div>
+        <hr className='h-[1px] w-100 bg-[#F1F3F4] lg:mt-6' />
+
+        <div className='flex flex-col justify-center text-center lg:px-44 pt-10'>
+          <h2 className='text-center text-[24px] lg:text-[32px] font-[700] leading-[32px] lg:leading-[40px] text-titleSubtitle mb-4'>
+            Want to implement AI diagnostics in your healthcare facility?
+          </h2>
+          <p className='text-center text-paragraph text-[20px] font-[400] leading-[32px]'>
+            Would you like any additional elements, such as a visual timeline or
+            success comparison charts?
+          </p>
+          <button className='sm:w-1/3 mx-auto mt-10 text-[14px] rounded-[28px] bg-primary text-white font-[600] leading-[24px] tracking-[1.4px] shadow-[0px_16px_16px_0px_rgba(25,188,229,0.20)] px-[24px] py-[10px] sm:py-[16px] uppercase'>
+            Get In Touch
+          </button>
+        </div>
       </div>
     </div>
   )
