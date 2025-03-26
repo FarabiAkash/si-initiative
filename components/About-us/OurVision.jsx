20aaefc0ae971355e539d3f1b1f1a882e8466f91
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-03-27 03:29:25 +0600

    Implement our vision in about-us page

---
+++ components/About-us/OurVision.jsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import visionImage from '../../public/assets/home/Blogs/blog1.png'
+
+const OurVision = () => {
+  return (
+    <div className='custom-container bg-gradient-to-bl from-white to-[#F5FDFF]'>
+      <div className='flex flex-col justify-center  items-center '>
+        <h1 className='text-titleSubtitle text-[32px] font-[700] leading-[40px]'>
+          Our Vision
+        </h1>
+        <p className='text-paragraph text-[18px] font-[400] leading-[24px] pt-3'>
+          Transforming Healthcare for a Better Tomorrow
+        </p>
+      </div>
+
+      <div className='flex flex-col lg:flex-row justify-center items-center gap-[20px] sm:gap-[40px]  mt-[50px]'>
+        <Image
+          src={visionImage}
+          alt='CMED Vision'
+          className='w-full h-full flex-1 rounded-[8px]'
+        />
+        <span className='text-[20px] sm:text-[24px] font-[400] leading-[36px] sm:leading-[40px] text-titleSubtitle text-justify'>
+          At CMED, we envision a world where technology bridges the gap in
+          healthcare, ensuring accessible, efficient, and data-driven medical
+          solutions for all. Through continuous innovation in AI, digital
+          health, and smart medical platforms, we strive to revolutionize
+          patient care, empower healthcare professionals, and enhance global
+          health outcomes.
+        </span>
+      </div>
+    </div>
+  )
+}
+export default OurVision
