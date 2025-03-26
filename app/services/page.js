d38e10be6a395ad42483d8c49bfd1e13ec05c008
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-03-27 05:03:08 +0600

    debug service page

--- app/services/page.js
+++ app/services/page.js
@@ -1,14 +1,14 @@
-import CoreService from "@/components/Services/CoreServices.jsx"
-import SuccessStories from "@/components/Home/SuccessStories.jsx"
-import PageHeader from "@/components/shared/PageHeader.jsx"
-import GetInTouch from "@/components/Services/GetInTouch.jsx"
-import SpecializedServices from "@/components/Services/SpecializedServices.jsx"
-import WorkProcess from "@/components/Services/WorkProcess.jsx"
+// import CoreService from "@/components/Services/CoreServices.jsx"
+// import SuccessStories from "@/components/Home/SuccessStories.jsx"
+// import PageHeader from "@/components/shared/PageHeader.jsx"
+// import GetInTouch from "@/components/Services/GetInTouch.jsx"
+// import SpecializedServices from "@/components/Services/SpecializedServices.jsx"
+// import WorkProcess from "@/components/Services/WorkProcess.jsx"
 
 const page = () => {
     return (
         <>
-            <PageHeader
+            {/* <PageHeader
                 title={'Innovative Healthcare Solutions for a Smarter Future'}
                 subtitle={'Empowering healthcare providers with AI-driven, data-centric, and digital solutions for better patient outcomesG'}
             />
@@ -16,7 +16,7 @@ const page = () => {
             <SpecializedServices />
             <WorkProcess />
             <SuccessStories pageName={'services'} />
-            <GetInTouch />
+            <GetInTouch /> */}
         </>
     )
 }
