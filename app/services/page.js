676b5279dd260073adf22fa44b6760406a4ea1f4
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-03-27 23:00:04 +0600

    Debugging in the production

--- app/services/page.js
+++ app/services/page.js
@@ -1,9 +1,4 @@
-import SpecializedServices from "@/components/Services/SpecializedServices"
-import CoreService from "../../components/Services/CoreService"
 import PageHeader from "../../components/shared/PageHeader"
-import WorkProcess from "../../components/Services/WorkProcess"
-import SuccessStories from "../../components/Home/SuccessStories"
-import GetInTouch from "../../components/Services/GetInTouch"
 
 const page = () => {
     return (
@@ -12,11 +7,11 @@ const page = () => {
                 title={'Innovative Healthcare Solutions for a Smarter Future'}
                 subtitle={'Empowering healthcare providers with AI-driven, data-centric, and digital solutions for better patient outcomesG'}
             />
-            <CoreService />
+            {/* <CoreService />
             <SpecializedServices />
             <WorkProcess />
             <SuccessStories pageName={'services'} />
-            <GetInTouch />
+            <GetInTouch /> */}
         </>
     )
 }
