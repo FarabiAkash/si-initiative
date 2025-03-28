dcb29e778faa894b7ca1b9dafa694e594551507c
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-03-28 23:47:26 +0600

    Implement contact

--- components/shared/Nav.jsx
+++ components/shared/Nav.jsx
@@ -10,12 +10,17 @@ const Nav = () => {
   const [isOpen, setIsOpen] = useState(false)
   const pathname = usePathname()
 
-  const isActive = path =>
+  const isActive = path => {
     pathname === path ? 'text-secondary' : 'text-[#6D787B]'
+  }
 
   return (
     <>
-      <nav className='flex items-center justify-between px-4 sm:px-12 xl:px-36  py-5'>
+      <nav
+        className={`flex items-center justify-between px-4 sm:px-12 xl:px-36  py-5 ${
+          pathname === '/contact-us' && 'bg-[#0B3641] !text-white'
+        }`}
+      >
         {/* Logo */}
         <Link href={'/'} className='w-[111.613px] h-[40px]'>
           <Image src={Logo} alt='SI-Initiative Logo' width={500} height={500} />
@@ -25,49 +30,61 @@ const Nav = () => {
         <div className='hidden lg:flex space-x-6'>
           <a
             href='/'
-            className={`hover:text-secondary text-[#6D787B] text-[14px] font-[500] ${isActive(
-              '/'
-            )}`}
+            className={`${
+              pathname === '/contact-us'
+                ? 'text-white'
+                : 'hover:text-secondary text-[#6D787B]'
+            }   text-[14px] font-[500] ${isActive('/')}`}
           >
             Home
           </a>
           <a
             href='/services'
-            className={`hover:text-secondary text-[#6D787B] text-[14px] font-[500] ${isActive(
-              '/services'
-            )}`}
+            className={`${
+              pathname === '/contact-us'
+                ? 'text-white'
+                : 'hover:text-secondary text-[#6D787B]'
+            }   text-[14px] font-[500] ${isActive('/services')}`}
           >
             Services
           </a>
           <a
             href='/case-studies'
-            className={`hover:text-secondary text-[#6D787B] text-[14px] font-[500] ${isActive(
-              '/case-studies'
-            )}`}
+            className={`${
+              pathname === '/contact-us'
+                ? 'text-white'
+                : 'hover:text-secondary text-[#6D787B]'
+            }   text-[14px] font-[500] ${isActive('/case-studies')}`}
           >
             Case Studies
           </a>
           <a
             href='/blogs'
-            className={`hover:text-secondary text-[#6D787B] text-[14px] font-[500] ${isActive(
-              '/blogs'
-            )}`}
+            className={`${
+              pathname === '/contact-us'
+                ? 'text-white'
+                : 'hover:text-secondary text-[#6D787B]'
+            }   text-[14px] font-[500] ${isActive('/blogs')}`}
           >
             Blogs
           </a>
           <a
             href='/about-us'
-            className={`hover:text-secondary text-[#6D787B] text-[14px] font-[500] ${isActive(
-              '/about-us'
-            )}`}
+            className={`${
+              pathname === '/contact-us'
+                ? 'text-white'
+                : 'hover:text-secondary text-[#6D787B]'
+            }   text-[14px] font-[500] ${isActive('/about-us')}`}
           >
             About Us
           </a>
           <a
             href='/contact-us'
-            className={`hover:text-secondary text-[#6D787B] text-[14px] font-[500] ${isActive(
-              '/contact-us'
-            )}`}
+            className={`${
+              pathname === '/contact-us'
+                ? 'text-white'
+                : 'hover:text-secondary text-[#6D787B]'
+            }   text-[14px] font-[500] ${isActive('/contact-us')}`}
           >
             Contact Us
           </a>
@@ -86,52 +103,68 @@ const Nav = () => {
 
       {/* Mobile Menu */}
       {isOpen && (
-        <div className='lg:hidden flex flex-col items-center space-y-4 bg-gray-100 py-5'>
+        <div
+          className={`lg:hidden flex flex-col items-center space-y-4 bg-gray-100 py-5 ${
+            pathname === '/contact-us' && 'bg-[#0B3641]'
+          }`}
+        >
           <a
             href='/'
-            className={`hover:text-secondary text-[#6D787B] text-[14px] font-[500] ${isActive(
-              '/'
-            )}`}
+            className={`${
+              pathname === '/contact-us'
+                ? 'text-white'
+                : 'hover:text-secondary text-[#6D787B]'
+            }   text-[14px] font-[500] ${isActive('/')}`}
           >
             Home
           </a>
           <a
             href='/services'
-            className={`hover:text-secondary text-[#6D787B] text-[14px] font-[500] ${isActive(
-              '/services'
-            )}`}
+            className={`${
+              pathname === '/contact-us'
+                ? 'text-white'
+                : 'hover:text-secondary text-[#6D787B]'
+            }   text-[14px] font-[500] ${isActive('/services')}`}
           >
             Services
           </a>
           <a
             href='/case-studies'
-            className={`hover:text-secondary text-[#6D787B] text-[14px] font-[500] ${isActive(
-              '/case-studies'
-            )}`}
+            className={`${
+              pathname === '/contact-us'
+                ? 'text-white'
+                : 'hover:text-secondary text-[#6D787B]'
+            }   text-[14px] font-[500] ${isActive('/case-studies')}`}
           >
             Case Studies
           </a>
           <a
             href='/blogs'
-            className={`hover:text-secondary text-[#6D787B] text-[14px] font-[500] ${isActive(
-              '/blogs'
-            )}`}
+            className={`${
+              pathname === '/contact-us'
+                ? 'text-white'
+                : 'hover:text-secondary text-[#6D787B]'
+            }   text-[14px] font-[500] ${isActive('/blogs')}`}
           >
             Blogs
           </a>
           <a
             href='/about-us'
-            className={`hover:text-secondary text-[#6D787B] text-[14px] font-[500] ${isActive(
-              '/about-us'
-            )}`}
+            className={`${
+              pathname === '/contact-us'
+                ? 'text-white'
+                : 'hover:text-secondary text-[#6D787B]'
+            }   text-[14px] font-[500] ${isActive('/about-us')}`}
           >
             About Us
           </a>
           <a
             href='/contact-us'
-            className={`hover:text-secondary text-[#6D787B] text-[14px] font-[500] ${isActive(
-              '/contact-us'
-            )}`}
+            className={`${
+              pathname === '/contact-us'
+                ? 'text-white'
+                : 'hover:text-secondary text-[#6D787B]'
+            }   text-[14px] font-[500] ${isActive('/contact-us')}`}
           >
             Contact Us
           </a>
