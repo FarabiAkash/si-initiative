70d3251e4b952f8bf5c389adcad2556a24cc8d4e
ShahinurAlamBhuiyan <shahinur.alam.bhuiyan01@gmail.com>
2025-10-19 20:10:16 +0600

    Frontend completion

--- app/portfolio/[id]/page.js
+++ app/portfolio/[id]/page.js
@@ -3,13 +3,14 @@ import { useState } from "react"
 import { useParams } from "next/navigation"
 import Link from "next/link"
 import PortfolioDetails from "@/components/Portfolio/PortfolioDetails"
+import ReadyToTransform from "@/components/shared/ReadyToTransform"
 
 const page = () => {
     const [selectedTag, setSelectedTag] = useState('All')
     const { id } = useParams()
 
     return (
-        <div > 
+        <div> 
             {/* Breadcrumb */}
             <div className='w-full h-[56px] flex justify-between gap-4 border-t border-b border-t-[#ffffff33] border-b-[#ffffff33]'>
             <p className='lg:w-1/5 py-[16px] leading-[24px] text-[16px] font-[500] flex justify-start items-center gap-[16px]'>
@@ -23,6 +24,7 @@ const page = () => {
             </p>
             </div>
             <PortfolioDetails />
+            <ReadyToTransform />
         </div>
     )
 }
